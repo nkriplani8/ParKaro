8b0d956a7b9d850c3151b633f05cac85c03534c0
finnynj <finny.nj@gmail.com>
2020-08-24 13:19:10 -0700

    Map init

--- app/src/main/java/com/example/parkaro/Activities/Dashboard.java
+++ app/src/main/java/com/example/parkaro/Activities/Dashboard.java
@@ -1,14 +1,25 @@
 package com.example.parkaro.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.parkaro.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.type.LatLng;
+import com.google.android.gms.maps.model.LatLng;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,25 @@ public class Dashboard extends AppCompatActivity {
 
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Map fragment support
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        int lat = 0, lng = 0;
+
+        //Map Styling
+        MapStyleOptions mapStyleOptions= MapStyleOptions.loadRawResourceStyle(this,R.raw.map_style);
+        googleMap.setMapStyle(mapStyleOptions);
+
+        LatLng p1 = new LatLng( (((double) lat / 18.518813)),
+                (((double) lng / 73.935187) ));
+
+        map.addMarker(new MarkerOptions().position(p1).title("Amanora Mall"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(p1));
     }
 }
